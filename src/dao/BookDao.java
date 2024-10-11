48ce0554060f5b9b765e5c0360a5730c0dbddec3
PC-02 <PC-02@host.docker.internal>
2024-10-11 14:47:33 +0900

    project

--- src/dao/BookDao.java
+++ src/dao/BookDao.java
@@ -1,5 +1,48 @@
 package dao;
 
-public class BookDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import util.DBUtil;
+import vo.BookVo;
+
+
+public class BookDao {
+	private Connection con = null;
+	private PreparedStatement ps = null;
+	private ResultSet rs = null;
+	
+	public List<BookVo> getbookList(){
+		List<BookVo> bookList = null;
+		
+		String sql = "SELECT A.BOARD_NO, A.TITLE, A.CONTENT, B.USER_NAME, A.REG_DATE"
+				+ " FROM TB_JDBC_BOARD A LEFT OUTER JOIN TB_JDBC_USER B"
+				+ " ON A.USER_ID = B.USER_ID"
+				+ " ORDER BY A.BOARD_NO DESC";
+		try {
+			con = DBUtil.getConnection();
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			bookList = new ArrayList<BookVo>();
+			while(rs.next()) {
+				BookVo bvo = new BookVo();
+				
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			disConnect();
+		}
+		return bookList;
+	}
+	private void disConnect() {
+		if(rs != null) try { rs.close(); } catch(Exception e) {}
+		if(ps != null) try { ps.close(); } catch(Exception e) {}
+		if(con != null) try { con.close(); } catch(Exception e) {}
+	}
 }
--- src/dao/LoanDao.java
+++ src/dao/LoanDao.java
@@ -1,5 +1,17 @@
 package dao;
 
-public class LoanDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+public class LoanDao {
+	
+	private Connection con = null;
+	private PreparedStatement ps = null;
+	private ResultSet rs = null;
+	private void disConnect() {
+		if(rs != null) try { rs.close(); } catch(Exception e) {}
+		if(ps != null) try { ps.close(); } catch(Exception e) {}
+		if(con != null) try { con.close(); } catch(Exception e) {}
+	}
 }

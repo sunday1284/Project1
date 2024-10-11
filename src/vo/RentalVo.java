e7ba19f7463fa26a3875a26df5890e2d8237104b
PC-02 <PC-02@host.docker.internal>
2024-10-11 20:15:00 +0900

    project

--- src/vo/RentalVo.java
+++ src/vo/RentalVo.java
@@ -2,45 +2,47 @@ package vo;
 
 public class RentalVo {
 	private String rental_date;
-	private String rental_name;
+	private String book_id;
 	private String rental_id;
-	
-	
+	private String mem_id;
+
+	@Override
+	public String toString() {
+		return "RentalVo [rental_date=" + rental_date + ", book_id=" + book_id + ", rental_id=" + rental_id
+				+ ", mem_id=" + mem_id + ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()="
+				+ super.toString() + "]";
+	}
+
 	public String getRental_date() {
 		return rental_date;
 	}
-
 
 	public void setRental_date(String rental_date) {
 		this.rental_date = rental_date;
 	}
 
-
-	public String getRental_name() {
-		return rental_name;
+	public String getBook_id() {
+		return book_id;
 	}
 
-
-	public void setRental_name(String rental_name) {
-		this.rental_name = rental_name;
+	public void setBook_id(String book_id) {
+		this.book_id = book_id;
 	}
-
 
 	public String getRental_id() {
 		return rental_id;
 	}
 
-
 	public void setRental_id(String rental_id) {
 		this.rental_id = rental_id;
 	}
 
-
-	@Override
-	public String toString() {
-		return "RentalVo [rental_date=" + rental_date + ", rental_name=" + rental_name + ", rental_id=" + rental_id
-				+ "]";
+	public String getMem_id() {
+		return mem_id;
 	}
-	
-	
+
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+
 }

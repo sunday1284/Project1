95b86d1a43183ebd41b76eca8773875cdf2d9254
PC-02 <PC-02@host.docker.internal>
2024-10-11 11:58:57 +0900

    project

---
+++ src/util/DBUtil.java
@@ -0,0 +1,31 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+// JDBC드라이버를 로딩하고 Connection객체를 생성하여 반환하는 메서드로 구성된 class
+public class DBUtil {
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (Exception e) {
+			System.out.println("드라이버 로딩 실패~~");
+			e.printStackTrace();
+		}
+	}
+	
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(
+					"jdbc:oracle:thin:@localhost:1588/FREEPDB1"
+					,"BOOKMANGE","java");
+		} catch (SQLException e) {
+			System.out.println("DB 연결 실패 ~~~");
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+}
